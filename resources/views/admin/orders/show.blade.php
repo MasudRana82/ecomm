5f3cd7ba12695412080dec87b2179453ca1d6049
Masud Rana (masudrana82) <33987725+MasudRana82@users.noreply.github.com>
2026-01-31 23:35:36 +0600

    order status edit to confirmed

--- resources/views/admin/orders/show.blade.php
+++ resources/views/admin/orders/show.blade.php
@@ -211,10 +211,10 @@
                                 class="w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500">
                                 <option value="pending" {{ $order->status === 'pending' ? 'selected' : '' }}>Pending
                                 </option>
-                                <option value="processing" {{ $order->status === 'processing' ? 'selected' : '' }}>
-                                    Processing</option>
-                                <option value="shipped" {{ $order->status === 'shipped' ? 'selected' : '' }}>Shipped
-                                </option>
+                                <option value="confirmed" {{ $order->status === 'confirmed' ? 'selected' : '' }}>
+                                    Confirmed</option>
+                                <!-- <option value="shipped" {{ $order->status === 'shipped' ? 'selected' : '' }}>Shipped
+                                </option> -->
                                 <option value="delivered" {{ $order->status === 'delivered' ? 'selected' : '' }}>Delivered
                                 </option>
                                 <option value="cancelled" {{ $order->status === 'cancelled' ? 'selected' : '' }}>Cancelled
